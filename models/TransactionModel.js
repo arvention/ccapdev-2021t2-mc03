b95ed43b7cf9832983463973cf7b30c406e16869
Arren Matthew Antioquia <amc.antioquia@yahoo.com.ph>
2021-05-10 00:47:54 +0800

    added models

---
+++ models/TransactionModel.js
@@ -0,0 +1,13 @@
+
+var mongoose = require('mongoose');
+
+/*
+    TODO:   Complete the TransactionSchema which will contain the name,
+            reference number, and the amount of a transaction in the database.
+*/
+
+var TransactionSchema = new mongoose.Schema({
+    // your code here
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
