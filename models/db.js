8ca8bc45b4a33bfe599b0e128d2936f45a65db2a
Arren Matthew Antioquia <amcan@ARREN-AIR-M1.local>
2023-07-21 12:12:47 +0800

    Updated to latest version of mongoose

--- models/db.js
+++ models/db.js
@@ -1,86 +1,99 @@
 
+// import module `mongoose`
 const mongoose = require('mongoose');
 
-const TransactionModel = require('./TransactionModel.js');
+// ccapdev-mco3 is the name of the database
+const url = 'mongodb://0.0.0.0:27017/ccapdev-mco3';
 
-const url = 'mongodb://localhost:27017/ccapdev-mc03';
-
+// additional connection options
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true
 };
 
+// defines an object which contains necessary database functions
 const database = {
 
-    connect: function () {
-        mongoose.connect(url, options, function(error) {
-            if(error) throw error;
-            console.log('Connected to: ' + url);
-        });
+    /*
+        connects to database
+    */
+    connect: async function () {
+        await mongoose.connect(url, options);
+        console.log('Connected to: ' + url);
     },
 
-    insertOne: function(model, doc, callback) {
-        model.create(doc, function(error, result) {
-            if(error) return callback(false);
-            console.log('Added ' + result);
-            return callback(true);
-        });
+    /*
+        inserts a single `doc` to the database based on the model `model`
+    */
+    insertOne: async function(model, doc) {
+        return await model.create(doc);
     },
 
-    insertMany: function(model, docs, callback) {
-        model.insertMany(docs, function(error, result) {
-            if(error) return callback(false);
-            console.log('Added ' + result);
-            return callback(true);
-        });
+    /*
+        inserts multiple `docs` to the database based on the model `model`
+    */
+    insertMany: async function(model, docs) {
+        return await model.insertMany(docs);
     },
 
-    findOne: function(model, query, projection, callback) {
-        model.findOne(query, projection, function(error, result) {
-            if(error) return callback(false);
-            return callback(result);
-        });
+    /*
+        searches for a single document based on the model `model`
+        filtered through the object `query`
+        limits the fields returned based on the string `projection`
+        callback function is called after the execution of findOne() function
+    */
+    findOne: async function(model, query, projection) {
+        return await model.findOne(query, projection);
     },
 
-    findMany: function(model, query, projection, callback) {
-        model.find(query, projection, function(error, result) {
-            if(error) return callback(false);
-            return callback(result);
-        });
+    /*
+        searches for multiple documents based on the model `model`
+        filtered through the object `query`
+        limits the fields returned based on the string `projection`
+        callback function is called after the execution of findMany() function
+    */
+    findMany: async function(model, query, projection) {
+        return await model.find(query, projection);
     },
 
-    updateOne: function(model, filter, update, callback) {
-        model.updateOne(filter, update, function(error, result) {
-            if(error) return callback(false);
-            console.log('Document modified: ' + result.nModified);
-            return callback(true);
-        });
+    /*
+        updates the value defined in the object `update`
+        on a single document based on the model `model`
+        filtered by the object `filter`
+    */
+    updateOne: async function(model, filter, update) {
+        return await model.updateOne(filter, update);
     },
 
-    updateMany: function(model, filter, update, callback) {
-        model.updateMany(filter, update, function(error, result) {
-            if(error) return callback(false);
-            console.log('Documents modified: ' + result.nModified);
-            return callback(true);
-        });
+    /*
+        updates the value defined in the object `update`
+        on multiple documents based on the model `model`
+        filtered using the object `filter`
+    */
+    updateMany: async function(model, filter, update) {
+        return await model.updateMany(filter, update);
     },
 
-    deleteOne: function(model, conditions, callback) {
-        model.deleteOne(conditions, function (error, result) {
-            if(error) return callback(false);
-            console.log('Document deleted: ' + result.deletedCount);
-            return callback(true);
-        });
+    /*
+        deletes a single document based on the model `model`
+        filtered using the object `conditions`
+    */
+    deleteOne: async function(model, conditions) {
+        return await model.deleteOne(conditions);
     },
 
-    deleteMany: function(model, conditions, callback) {
-        model.deleteMany(conditions, function (error, result) {
-            if(error) return callback(false);
-            console.log('Document deleted: ' + result.deletedCount);
-            return callback(true);
-        });
+    /*
+        deletes multiple documents based on the model `model`
+        filtered using the object `conditions`
+    */
+    deleteMany: async function(model, conditions) {
+        return await model.deleteMany(conditions);
     }
 
 }
 
+/*
+    exports the object `database` (defined above)
+    when another script exports from this file
+*/
 module.exports = database;
